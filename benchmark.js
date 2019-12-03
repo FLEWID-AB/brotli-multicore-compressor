948fd9b07108e172749958f11658d11788e47d57
Philip Mannheimer <philip.mannheimer@gmail.com>
2019-12-03 18:19:51 +0100

    added elapsed time

--- benchmark.js
+++ benchmark.js
@@ -1,22 +1,26 @@
 #!/usr/bin/env node
 const argv = require('yargs').argv
 
-const walk        = require('walk');
-const path        = require('path');
-const workerFarm  = require('worker-farm')
-const workers     = workerFarm(require.resolve('./compress.js'))
-const colors      = require('colors')
+const walk = require('walk');
+const path = require('path');
+const workerFarm = require('worker-farm')
+const workers = workerFarm(require.resolve('./compress.js'))
+const compressMe = require('./compress.js')
+const colors = require('colors')
 
 const os = require('os')
 const fs = require('fs')
 const FindFiles = require('file-regex');
 const fileSize = require("filesize");
+
 const { TaskTimer } = require('tasktimer');
 const timer = new TaskTimer(10);
+const timer2 = new TaskTimer(10);
+// timer.on('tick', () => console.log(`Tick count: ${timer.tickCount}`));
 
 
-const listAllFiles = async(dir) => {
-  timer.start()
+const listAllFiles = async (dir) => {
+  timer.start();
   var files = [];
 
   // Walker options
@@ -24,9 +28,9 @@ const listAllFiles = async(dir) => {
 
   walker.on('file', function (root, stat, next) {
     let ext_regex = /\.(svg|html|json|js|css|xml|htm)/
-    let br_regex  = /\.br\./
+    let br_regex = /\.br\./
     // Add this file to the list of files
-    if (!stat.name.match(br_regex) && path.extname(stat.name).match(ext_regex)){
+    if (!stat.name.match(br_regex) && path.extname(stat.name).match(ext_regex)) {
       files.push(root + '/' + stat.name);
     }
     next();
@@ -39,15 +43,12 @@ const listAllFiles = async(dir) => {
     for (let index = 0; index < files.length; index++) {
       const element = files[index];
       workers(element, (err, outp) => {
-        console.log(colors.green("Compressed: " + outp.filePath))
-        console.log(colors.grey("Original size: " + outp.originalFileSize + " | " + "Brotli Size: " + outp.brotliFileSize + " | " + "Saved: " + outp.difference))
-        console.log("\n")
-        if(index >= files.length-1) {
+        if (index >= files.length - 1) {
           workerFarm.end(workers, (callb) => {
-            getTotalFileSize().then(() => {
-              timer.stop()
-              console.log(colors.grey('Elapsed time: ' + timer.time.elapsed + ' ms.'));
-            })
+            getTotalFileSize();
+            timer.stop();
+            console.log('elapsed time: ' + timer.time.elapsed + ' ms.');
+            listAllFilesSync(dir)
           });
         }
       });
@@ -55,10 +56,45 @@ const listAllFiles = async(dir) => {
   });
 }
 
+const listAllFilesSync = async (dir) => {
+  timer2.start()
+  var files = [];
+
+  // Walker options
+  var walker = walk.walk(dir, { followLinks: false });
+
+  walker.on('file', function (root, stat, next) {
+    let ext_regex = /\.(svg|html|json|js|css|xml|htm)/
+    let br_regex = /\.br\./
+    // Add this file to the list of files
+    if (!stat.name.match(br_regex) && path.extname(stat.name).match(ext_regex)) {
+      files.push(root + '/' + stat.name);
+    }
+    next();
+  });
+
+  walker.on('end', function () {
+    console.log("\n")
+    console.log(colors.blue("Processing " + files.length + " files..."))
+    console.log("\n")
+    for (let index = 0; index < files.length; index++) {
+      const element = files[index];
+      compressMe(element, (data) => {
+
+      })
+    }
+    getTotalFileSize()
+    timer2.stop();
+    console.log('elapsed time: ' + timer2.time.elapsed + ' ms.');
+  });
+}
+
 const argv_dir = argv.dir
 
-if (argv_dir){
+if (argv_dir) {
   listAllFiles(argv_dir)
+  // listAllFilesSync(argv_dir)
+
 } else {
   console.log(colors.red("ERROR: No --dir was set."))
 }
@@ -69,7 +105,7 @@ const getTotalFileSize = async () => {
   let originalTotalFileSize = 0;
   let brotiTotalFileSize = 0;
 
-  const originalFiles = await FindFiles("./tests/files", /^((?!\.br).)*\.(svg|html|json|js|css|xml|htm)$/gm, 9999, { concurrency: os.cpus().length})
+  const originalFiles = await FindFiles("./tests/files", /^((?!\.br).)*\.(svg|html|json|js|css|xml|htm)$/gm, 9999, { concurrency: os.cpus().length })
   for (let index = 0; index < originalFiles.length; index++) {
     originalTotalFileSize += fs.statSync(originalFiles[index].dir + "/" + originalFiles[index].file).size;
   }
